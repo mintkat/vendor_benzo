a27d04589f5fa1e66fd90c772303395f780f1eaa
mydongistiny <jaysonedson@gmail.com>
2015-12-03 14:51:59 -0800

    vendor: Build kernel using SaberMod 5.2.1

--- config/optimizations/optimizations.mk
+++ config/optimizations/optimizations.mk
@@ -30,7 +30,7 @@ endif
 export TARGET_NDK_GCC_VERSION := 4.9
 export TARGET_GCC_VERSION_EXP := 4.9
 export TARGET_GCC_VERSION := 4.9
-export TARGET_GCC_VERSION_KERNEL := 4.9
+export TARGET_GCC_VERSION_KERNEL := 5.2
 
 # benzoCore
 export KBUILD_BUILD_USER := xanaxdroid
