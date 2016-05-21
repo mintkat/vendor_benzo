eead94a22d31d1a1f69b1782acbcd661d6c1dbc0
mintkat <melpri@hotmail.com>
2016-05-22 01:42:17 +0200

    fixed libjni

--- config/common.mk
+++ config/common.mk
@@ -75,8 +75,8 @@ PRODUCT_COPY_FILES += \
 
 # Copy latinime for gesture typing
 PRODUCT_COPY_FILES += \
-    vendor/benzo/prebuilt/common/lib64/libjni_latinime.so:system/lib64/libjni_latinime.so \
-    vendor/benzo/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so
+    vendor/benzo/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so \
+    vendor/benzo/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 
 # Layers backup
 PRODUCT_COPY_FILES += \
